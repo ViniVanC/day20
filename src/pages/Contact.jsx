b976dc5983081d04a3f94ae4f545454601721ccd
ViniVanC <vincentvan0302@gmail.com>
2023-03-29 18:06:37 +0300

    created pages

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box } from "../components/Box";
+import { Container } from "../components/Container";
+
+export const Contact = () => {
+  return (
+    <div className="contact-page">
+      <Container>
+        <Box>
+          <h2 className="title">
+            Вітаю. <br /> Пропоную тобі вступити до лав нашої організації.
+          </h2>
+          <p>
+            Для цього просто залиште заявку, заповнивши відповідну форму нижче.
+          </p>
+          <form className="form">
+            <input type="text" placeholder="ім'я" />
+            <input type="text" placeholder="вік" />
+            <textarea placeholder="навички"></textarea>
+            <button>надіслати</button>
+          </form>
+          <hr />
+        </Box>
+        <Box>
+          <h2 className="title">Бачили щось аномальне? Повідомте нас.</h2>
+          <p>
+            Якщо ви, чи ваші близькі бачили щось аномальне, паранормальне,
+            незвичайне, дивне. Обов'язково повідомляйте нас про це.
+          </p>
+          <form className="form">
+            <input type="text" placeholder="ім'я" />
+            <textarea placeholder="опишіть що ви бачили"></textarea>
+            <button>надіслати</button>
+          </form>
+        </Box>
+      </Container>
+    </div>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "../components/Container";
+
+export const Home = () => {
+  return (
+    <div>
+      <Container>hi</Container>
+    </div>
+  );
+};
