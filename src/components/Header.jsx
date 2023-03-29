7bd724c27da1cdc83b1e077f6fb42b0aab5d325e
ViniVanC <vincentvan0302@gmail.com>
2023-03-29 19:23:57 +0300

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,20 @@ import { Navbar } from "./Navbar";
 
 export const Header = () => {
   return (
-    <header className="header">
-      <Container>
-        <div className="inner">
-          <Logo />
-          <Navbar />
-        </div>
-      </Container>
-    </header>
+    <>
+      <header className="header">
+        <Container>
+          <div className="inner">
+            <Logo />
+            <Navbar />
+          </div>
+        </Container>
+      </header>
+      <hr
+        style={{
+          margin: "0",
+        }}
+      />
+    </>
   );
 };
