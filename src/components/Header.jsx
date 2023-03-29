38efd10f8dd1a422b1c26d439e95cfaba5aa56bf
ViniVanC <vincentvan0302@gmail.com>
2023-03-29 18:50:27 +0300

    added Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Logo } from "./Logo";
+import { Container } from "./Container";
+import { Navbar } from "./Navbar";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <Container>
+        <div className="inner">
+          <Logo />
+          <Navbar />
+        </div>
+      </Container>
+    </header>
+  );
+};
