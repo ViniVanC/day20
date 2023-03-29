1956af3933f12e18147ec643e5ef9261f2a8fa83
ViniVanC <vincentvan0302@gmail.com>
2023-03-29 18:49:37 +0300

    created Logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Logo = () => {
+  return (
+    <Link to="/" className="logo">
+      <span></span>
+      <span></span>
+      <span></span>
+    </Link>
+  );
+};
